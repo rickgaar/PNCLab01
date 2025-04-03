3efea831ca4ec0e8b38b343cea7ab90b674f9317
Ricardo Garcia <00148922@uca.edu.sv>
2025-04-03 12:30:55 -0600

    add: classes

---
+++ Labo01/src/Libro.java
@@ -0,0 +1,15 @@
+class Libro {
+    String id;
+    String nombre;
+    String autor;
+    int anio;
+    String genero;
+
+    public Libro (String id, String nombre, String autor, int anio, String genero){
+        this.id = id;
+        this.nombre = nombre;
+        this.autor = autor;
+        this.anio = anio;
+        this.genero = genero;
+    }
+}
