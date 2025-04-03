ca2617cfbd0497ecbdd30a5b8bf2373403b41e7f
Ricardo Garcia <00148922@uca.edu.sv>
2025-04-03 13:05:33 -0600

    add: Libro Methods

--- Labo01/src/classes/Libro.java
+++ Labo01/src/classes/Libro.java
@@ -8,11 +8,24 @@ public class Libro {
     String genero;
     boolean prestado;
 
-    public Libro (String id, String nombre, String autor, int anio, String genero){
+    public Libro (String id, String nombre, String autor, int anio, String genero, boolean prestado){
         this.id = id;
         this.nombre = nombre;
         this.autor = autor;
         this.anio = anio;
         this.genero = genero;
+        this.prestado = prestado;
+    }
+
+    public boolean estaDisponible(){
+        return !prestado;
+    }
+
+    public void cambiarEstado(){
+        this.prestado = this.!prestado;
+    }
+
+    public String getEstado(){
+        return prestado;
     }
 }
